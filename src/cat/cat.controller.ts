4478b5a989ca9cfd5862ffa8d6e8c1609a1780ee
Gwemin <dbwnghks921@naver.com>
2023-05-21 13:17:08 +0000

    redirect, response 처리

---
+++ src/cat/cat.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Query,
+  Redirect,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Controller('cat')
+export class CatController {
+  @Post()
+  create(@Res() res: Response) {
+    res.status(HttpStatus.CREATED).send();
+  }
+
+  @Get()
+  findAll(@Res() res: Response) {
+    res.status(HttpStatus.OK).json([]);
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+}
